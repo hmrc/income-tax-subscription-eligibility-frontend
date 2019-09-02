359621ab4e50e3165ea0db362fd465a6c799b141
Valentin Lukovski <47025947+VLukovski@users.noreply.github.com>
2019-09-02 16:03:40 +0100

    Created get software view and controller

---
+++ it/uk/gov/hmrc/incometaxsubscriptioneligibilityfrontend/controllers/GetSoftwareControllerISpec.scala
@@ -0,0 +1,56 @@
+
+package uk.gov.hmrc.incometaxsubscriptioneligibilityfrontend.controllers
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.scalatestplus.play.guice.GuiceOneServerPerSuite
+import play.api.test.Helpers._
+import uk.gov.hmrc.incometaxsubscriptioneligibilityfrontend.assets.MessageLookup.{GetSoftware => messages}
+import uk.gov.hmrc.incometaxsubscriptioneligibilityfrontend.utils.{ComponentSpecBase, ViewSpec}
+
+
+class GetSoftwareControllerISpec extends ComponentSpecBase with ViewSpec with GuiceOneServerPerSuite {
+
+  lazy val result = get("/get-software")
+  lazy val doc: Document = Jsoup.parse(result.body)
+
+  "GET /get-software" should {
+    "return OK" in {
+      result must have(
+        httpStatus(OK)
+      )
+    }
+
+    "have the correct title" in {
+      doc.title mustBe messages.title
+    }
+
+    "have the correct header" in {
+      doc.getH1Element.text mustBe messages.header
+    }
+
+    "have the correct text" in {
+      doc.getParagraphs must have(
+        elementWithValue(messages.line1)
+      )
+    }
+
+    "have the continue button" in {
+      doc.getSubmitButton.text() mustBe messages.continue
+    }
+
+    "have the correct hyperlink" in {
+      doc.getLink("find-software-link") must have(
+        text(messages.findSoftware),
+        href(messages.findSoftwareLink)
+      )
+    }
+
+    "have the correct form" in {
+      doc.getForm must have(
+        method("GET"),
+        action(routes.TermsController.show().url)
+      )
+    }
+  }
+}
