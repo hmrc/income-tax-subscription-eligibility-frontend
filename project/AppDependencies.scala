50dc86e1b3237f36b3ed168c178a39fee5488468
scully.thornton <44894097+scullythornton1@users.noreply.github.com>
2020-09-25 12:32:26 +0100

    MISUV-935-1: update scala to 2.12.12 ans sbt to 1.3.13 and dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -15,8 +15,8 @@ object AppDependencies {
 
   val test = Seq(
     "uk.gov.hmrc" %% "bootstrap-play-26" % "0.45.0" % Test classifier "tests",
-    "org.scalatest" %% "scalatest" % "3.0.8" % "test",
-    "org.jsoup" % "jsoup" % "1.10.2" % "test, it",
+    "org.scalatest" %% "scalatest" % "3.0.9" % "test",
+    "org.jsoup" % "jsoup" % "1.10.3" % "test, it",
     "com.typesafe.play" %% "play-test" % current % "test",
     "org.pegdown" % "pegdown" % "1.6.0" % "test, it",
     "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % "test, it"
