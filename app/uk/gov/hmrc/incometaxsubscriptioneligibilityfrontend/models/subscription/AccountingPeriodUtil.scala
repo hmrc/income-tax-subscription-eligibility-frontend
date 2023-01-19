796660a1205b27c35fb0c1c88a4bf65d320b8e92
Edward Rowles <justin.rowles@gmail.com>
2023-01-19 16:30:50 +0000

    [ITSASU-2036] Add 'Confirm client details' section

---
+++ app/uk/gov/hmrc/incometaxsubscriptioneligibilityfrontend/models/subscription/AccountingPeriodUtil.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.incometaxsubscriptioneligibilityfrontend.models.subscription
+
+import models.DateModel
+import models.common.AccountingPeriodModel
+
+import java.time.LocalDate
+
+object AccountingPeriodUtil {
+  private val april = 4
+  private val fifth = 5
+  private val sixth = 6
+
+  def getTaxStartYear(date: LocalDate): Int =
+    if (date.isAfter(LocalDate.of(date.getYear, april, fifth))) {
+      date.getYear
+    } else {
+      date.getYear - 1
+    }
+
+  def getTaxEndYear(date: LocalDate): Int =
+    if (date.isBefore(LocalDate.of(date.getYear, april, sixth))) {
+      date.getYear
+    } else {
+      date.getYear + 1
+    }
+
+  def getTaxStartYear(accountingPeriodModel: AccountingPeriodModel): Int = getTaxStartYear(accountingPeriodModel.startDate.toLocalDate)
+  def getTaxEndYear(accountingPeriodModel: AccountingPeriodModel): Int = getTaxEndYear(accountingPeriodModel.endDate.toLocalDate)
+
+  def getCurrentTaxEndYear: Int = getTaxEndYear(LocalDate.now())
+
+  def getCurrentTaxYearStartDate: DateModel = DateModel(sixth.toString, april.toString, (getCurrentTaxEndYear - 1).toString)
+  def getCurrentTaxYearEndDate: DateModel = DateModel(fifth.toString, april.toString, getCurrentTaxEndYear.toString)
+
+  def getCurrentTaxYear: AccountingPeriodModel = AccountingPeriodModel(getCurrentTaxYearStartDate, getCurrentTaxYearEndDate)
+  def getNextTaxYear: AccountingPeriodModel = AccountingPeriodModel(getCurrentTaxYearStartDate.plusYears(1), getCurrentTaxYearEndDate.plusYears(1))
+
+  def getCurrentTaxYearRange: String = s"${getCurrentTaxYear.taxStartYear}-${getCurrentTaxYear.taxEndYearShort}"
+  def getNextTaxYearRange: String = s"${getNextTaxYear.taxStartYear}-${getNextTaxYear.taxEndYearShort}"
+
+  implicit object LocalDateOrdering extends Ordering[LocalDate] {
+    override def compare(x: LocalDate, y: LocalDate): Int = x.compareTo(y)
+  }
+
+}
