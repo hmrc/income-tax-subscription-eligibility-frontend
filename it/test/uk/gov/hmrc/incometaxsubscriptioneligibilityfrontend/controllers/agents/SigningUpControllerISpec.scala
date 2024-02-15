5fce58f2f08e7338345c2af5565a61fcd6a9abab
Alex Rimmer <31283208+AlexRimmerHMRC@users.noreply.github.com>
2024-02-15 15:12:15 +0000

    ITSASU-2881 - Update content for the agent eligibility interrupt page

--- it/test/uk/gov/hmrc/incometaxsubscriptioneligibilityfrontend/controllers/agents/SigningUpControllerISpec.scala
+++ it/test/uk/gov/hmrc/incometaxsubscriptioneligibilityfrontend/controllers/agents/SigningUpControllerISpec.scala
@@ -17,13 +17,12 @@
 package uk.gov.hmrc.incometaxsubscriptioneligibilityfrontend.controllers.agents
 
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
+import org.jsoup.nodes.{Document, Element}
 import play.api.libs.ws.WSResponse
 import play.api.test.Helpers._
-import uk.gov.hmrc.incometaxsubscriptioneligibilityfrontend.assets.MessageLookup.AgentSignupTerms.AgentHeading._
-import uk.gov.hmrc.incometaxsubscriptioneligibilityfrontend.assets.MessageLookup.AgentSignupTerms.AgentHowToSignUp.AgentCompleteSignUp
+import uk.gov.hmrc.incometaxsubscriptioneligibilityfrontend.assets.MessageLookup.AgentSignupTerms.HowToSignUp.{CheckEligibility, CompleteSignUp, ConfirmSignUp, GetReady}
 import uk.gov.hmrc.incometaxsubscriptioneligibilityfrontend.assets.MessageLookup.AgentSignupTerms._
-import uk.gov.hmrc.incometaxsubscriptioneligibilityfrontend.assets.MessageLookup.{Base, agentSuffix}
+import uk.gov.hmrc.incometaxsubscriptioneligibilityfrontend.assets.MessageLookup.Base
 import uk.gov.hmrc.incometaxsubscriptioneligibilityfrontend.utils.{ComponentSpecBase, ViewSpec}
 
 class SigningUpControllerISpec extends ComponentSpecBase with ViewSpec {
@@ -42,177 +41,175 @@ class SigningUpControllerISpec extends ComponentSpecBase with ViewSpec {
     }
 
     "return a view" which {
-      "has a title" in {
-        doc.title mustBe s"${AgentHeading.heading}$agentSuffix"
-      }
-
-      "return a view" which {
-        "uses the correct template details" in new TemplateViewTest(
-          document = doc,
-          title = heading,
-          isAgent = true,
-          backLink = Some(appConfig.govukGuidanceITSASignUpAgentLink)
-        )(appConfig)
-      }
-
-      "return a view with a title" in {
-        doc.title mustBe s"$heading$agentSuffix"
-      }
-
+      "uses the correct template details" in new TemplateViewTest(
+        document = doc,
+        title = Heading.heading,
+        isAgent = true,
+        backLink = Some(appConfig.govukGuidanceITSASignUpAgentLink)
+      )(appConfig)
 
       "has main content" which {
         val mainContent = doc.getElementById("main-content")
         "has a heading section" which {
-          import AgentHeading._
           "has a heading" in {
-            mainContent.getH1Element.text mustBe heading
+            mainContent.getH1Element.text mustBe Heading.heading
           }
-          "has a first para" in {
-            mainContent.select("p").get(0).text mustBe para1
-          }
-
-          "has a second para" in {
-            mainContent.select("p").get(1).text mustBe para2
+          "has a income paragraph" in {
+            mainContent.selectNth("p", 1).text mustBe Heading.haveIncomeTypePara
           }
 
-          "has a first bullet point" in {
-            mainContent.selectFirst("ul").select("li").get(0).text mustBe bullet1
-          }
-          "has a second bullet point" in {
-            mainContent.selectFirst("ul").select("li").get(1).text mustBe bullet2
-          }
-
-          "has a third para" in {
-            mainContent.select("p").get(2).text mustBe para3
-          }
-
-        }
-
-        "has a how to sign up section" which {
-          "has a heading section" which {
-          }
-
-          "has a Get your clients ready section" which {
-            import AgentHowToSignUp.AgentGetReady._
-            "has a heading" in {
-              mainContent.select("h3").get(0).text mustBe heading
-            }
-            "has a para" in {
-              mainContent.select("p").get(3).text mustBe para
-            }
-          }
-
-          "has a Check their eligibility section" which {
-            import AgentHowToSignUp.AgentCheckAvailability._
-            "has a heading" in {
-              mainContent.select("h3").get(1).text mustBe heading
-            }
-            "has a para" in {
-              mainContent.select("p").get(4).text mustBe para
-            }
-
-          }
-
-          "has a Get your client’s information ready section" which {
-            import AgentHowToSignUp.AgentGetInformation._
-            "has a heading" in {
-              mainContent.select("h3").get(2).text mustBe heading
-            }
-
-            "has a para" in {
-              mainContent.select("p").get(5).text mustBe para
-            }
+          "has a bullet list about income" which {
+            def bulletList: Element = mainContent.selectNth("ul", 1)
 
             "has a first bullet point" in {
-              mainContent.selectNth("ul", 2).select("li").get(0).text mustBe bullet1
+              bulletList.selectNth("li", 1).text mustBe Heading.haveIncomeTypeBullet1
             }
             "has a second bullet point" in {
-              mainContent.selectNth("ul", 2).select("li").get(1).text mustBe bullet2
-            }
-            "has a third bullet point" in {
-              mainContent.selectNth("ul", 2).select("li").get(2).text mustBe bullet3
-            }
-            "has a fourth bullet point" in {
-              mainContent.selectNth("ul", 2).select("li").get(3).text mustBe bullet4
+              bulletList.selectNth("li", 2).text mustBe Heading.haveIncomeTypeBullet2
             }
           }
 
-          "has a Confirm client details section" which {
-            import AgentHowToSignUp.AgentConfirmClientDetails._
-            "has a heading" in {
-              mainContent.select("h3").get(3).text mustBe heading
-            }
+          "has a paragraph about accounting period" in {
+            mainContent.selectNth("p", 2).text mustBe Heading.accountingPeriodPara
+          }
 
-            "has a first para" in {
-              mainContent.select("p").get(6).text mustBe para1
-            }
-
-            "has a second para" in {
-              mainContent.select("p").get(7).text mustBe para2
-            }
+          "has a bullet list about accounting period" which {
+            def bulletList: Element = mainContent.selectNth("ul", 2)
 
             "has a first bullet point" in {
-              mainContent.selectFirst("ol").selectNth("ul", 2).select("li").get(0).text mustBe bullet1
+              bulletList.selectNth("li", 1).text mustBe Heading.accountingPeriodBullet1
             }
             "has a second bullet point" in {
-              mainContent.selectFirst("ol").selectNth("ul", 2).select("li").get(1).text mustBe bullet2
+              bulletList.selectNth("li", 2).text mustBe Heading.accountingPeriodBullet2
             }
+          }
 
-            "has a first inset para" in {
-              mainContent.selectNth("p", 9).text mustBe para3
-            }
+        }
 
-            "has a second inset para" in {
-              mainContent.selectNth("p", 10).text mustBe para4
+        "has a how to sign up detail block" which {
+          def details: Element = mainContent.selectHead("details")
+
+          "has a summary" in {
+            details.selectHead("summary").text mustBe HowToSignUp.heading
+          }
+
+          "has the details body" which {
+            def detailsBody: Element = details.selectHead("div")
+
+            "has numbered list" which {
+              def numberedList: Element = detailsBody.selectHead("ol")
+
+              "has a first list item (get ready to sign up)" which {
+                def listItem: Element = numberedList.selectHead("ol > li:nth-of-type(1)")
+
+                "has a heading" in {
+                  listItem.selectHead("h2").text mustBe GetReady.heading
+                }
+                "has a must have para" in {
+                  listItem.selectNth("p", 1).text mustBe GetReady.mustHavePara
+                }
+                "has a bullet list for the agents must have" which {
+                  def bulletList: Element = listItem.selectNth("ul", 1)
+
+                  "has a first bullet" in {
+                    bulletList.selectNth("li", 1).text mustBe GetReady.mustHaveBullet1
+                  }
+                  "has a second bullet" in {
+                    bulletList.selectNth("li", 2).text mustBe GetReady.mustHaveBullet2
+                  }
+                }
+                "has a client has software para" in {
+                  listItem.selectNth("p", 2).text mustBe GetReady.clientSoftwarePara
+                }
+                "has a details required for each client para" in {
+                  listItem.selectNth("p", 3).text mustBe GetReady.eachClientPara
+                }
+                "has a bullet list about required details for each client" which {
+                  def bulletList: Element = listItem.selectNth("ul", 2)
+
+                  "has a first bullet" in {
+                    bulletList.selectNth("li", 1).text mustBe GetReady.eachClientName
+                  }
+                  "has a second bullet" in {
+                    bulletList.selectNth("li", 2).text mustBe GetReady.eachClientNino
+                  }
+                  "has a third bullet" in {
+                    bulletList.selectNth("li", 3).text mustBe GetReady.eachClientDOB
+                  }
+                  "has a forth bullet" in {
+                    bulletList.selectNth("li", 4).text mustBe GetReady.eachClientBusinessDetails1
+                  }
+                  "has a fifth bullet" in {
+                    bulletList.selectNth("li", 5).text mustBe GetReady.eachClientBusinessDetails2
+                  }
+                  "has a sixth bullet" in {
+                    bulletList.selectNth("li", 6).text mustBe GetReady.eachClientBusinessDetails3
+                  }
+                }
+              }
+              "has a second list item (check eligibility)" which {
+                def listItem: Element = numberedList.selectHead("ol > li:nth-of-type(2)")
+
+                "has a heading" in {
+                  listItem.selectHead("h2").text mustBe CheckEligibility.heading
+                }
+                "has a para for we will check eligibility" in {
+                  listItem.selectHead("p").text mustBe CheckEligibility.para
+                }
+                "has an inset text section" which {
+                  "has content for if their client is not eligible" in {
+                    listItem.selectHead(".govuk-inset-text").text mustBe CheckEligibility.cantSignUp
+                  }
+                }
+              }
+              "has a third list item (complete sign up)" which {
+                def listItem: Element = numberedList.selectHead("ol > li:nth-of-type(3)")
+
+                "has a heading" in {
+                  listItem.selectHead("h2").text mustBe CompleteSignUp.heading
+                }
+                "has a para" in {
+                  listItem.selectHead("p").text mustBe CompleteSignUp.para
+                }
+                "has a bullet list" which {
+                  def bulletList: Element = listItem.selectHead("ul")
+
+                  "has a first bullet" in {
+                    bulletList.selectNth("li", 1).text mustBe CompleteSignUp.bullet1
+                  }
+                  "has a second bullet" in {
+                    bulletList.selectNth("li", 2).text mustBe CompleteSignUp.bullet2
+                  }
+                }
+              }
+              "has a forth list item (confirm sign up)" which {
+                def listItem: Element = numberedList.selectHead("ol > li:nth-of-type(4)")
+
+                "has a heading" in {
+                  listItem.selectHead("h2").text mustBe ConfirmSignUp.heading
+                }
+                "has a first para" in {
+                  listItem.selectNth("p", 1).text mustBe ConfirmSignUp.para1
+                }
+                "has a second para" in {
+                  listItem.selectNth("p", 2).text mustBe ConfirmSignUp.para2
+                }
+              }
             }
           }
         }
 
-        "has a Complete Sign Up section" which {
-          import AgentHowToSignUp.AgentCompleteSignUp._
-          "has a heading" in {
-            mainContent.select("h3").get(4).text mustBe heading
-          }
 
-          "has a para" in {
-            mainContent.select("p").get(10).text mustBe AgentCompleteSignUp.para
-          }
-
-          "has a first bullet point" in {
-            mainContent.select("ol").select("ul").get(2).select("li").get(0).text mustBe bullet1
-          }
-
-          "has a second bullet point" in {
-            mainContent.select("ol").select("ul").get(2).select("li").get(1).text mustBe bullet2
-          }
-
-          "has a third bullet point" in {
-            mainContent.select("ol").select("ul").get(2).select("li").get(2).text mustBe bullet3
-          }
+        "has a continue button" in {
+          doc.select("button").last().text mustBe Base.continue
         }
 
-        "has a Confirm sign-up section" which {
-          import AgentHowToSignUp.AgentConfirm._
-          "has a heading" in {
-            mainContent.select("h3").get(5).text mustBe heading
-          }
-
-          "has a paragraph" in {
-            mainContent.selectNth("p", 12).text mustBe para
-          }
-
+        "has the correct form" in {
+          doc.getForm must have(
+            method("POST"),
+            action(routes.SigningUpController.submit.url)
+          )
         }
-      }
-
-      "has a continue button" in {
-        doc.select("button").last().text mustBe Base.continue
-      }
-
-      "has the correct form" in {
-        doc.getForm must have(
-          method("POST"),
-          action(routes.SigningUpController.submit.url)
-        )
       }
     }
   }
